9be86b9f5c998a9b6bbfcb6503de36cdcc32d702
adilsachwani <adilsachwani@gmail.com>
2018-08-25 10:23:32 +0500

    events

--- multicast_events.cs
+++ multicast_events.cs
@@ -7,8 +7,12 @@ class EventDemo
     static void Main(string[] args)
     {
         MyEvent evt = new MyEvent();
+        X x = new X();
+        Y y = new Y();
 
         evt.SomeEvent += Handler;
+        evt.SomeEvent += x.XHandler;
+        evt.SomeEvent += y.YHandler;
 
         evt.onSomeEvent();
 
@@ -27,5 +31,19 @@ class MyEvent
     public void onSomeEvent(){
         if (SomeEvent != null)
             SomeEvent();
+    }
+}
+
+class X
+{
+    public void XHandler(){
+        Console.WriteLine("Event received by X Object");
+    }
+}
+
+class Y
+{
+    public void YHandler(){
+        Console.WriteLine("Event received by Y Object");
     }
 }
